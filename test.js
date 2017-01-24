e9c63c932863560609a501b0e0fcb5f3d3fac2b5
Rebecca Hill <beerecca@gmail.com>
2017-01-24 20:57:11 +0100

    nom

--- test.js
+++ test.js
@@ -2,78 +2,65 @@ const _ = require('lodash');
 
 //NORMAL FUNCTIONS
 (function() {
+  const sauce = 'sauce';
 
-	const sauce = 'sauce';
+  const lasagne = function(firstIngredient, secondIngredient) {
+    return firstIngredient + ' ' + secondIngredient;
+  }
 
-	const lasagne = function(firstIngredient, secondIngredient) {
-		return firstIngredient + ' ' + secondIngredient;
-	}
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('pasta')}, 1000);
+  });
 
-  	const promise = new Promise((resolve, reject) => {
-  		setTimeout(
-  		    () => {resolve('pasta')}, 
-  		    1000);
-  	});
-		
-		
-  	promise
-  		.then(result => lasagne(sauce, result))
-  		.then((result) => {
-  			console.log('1. yum!', result);
-	 	});
+  promise
+    .then(result => lasagne(sauce, result))
+    .then((result) => {
+      console.log('1. yum!', result);
+    });
 
 })();
 
 
 //LODASH CURRY
 (function() {
+  const sauce = 'sauce';
 
-	const sauce = 'sauce';
+  const lasagne = _.curry(function(firstIngredient, secondIngredient) {
+    return firstIngredient + ' ' + secondIngredient;
+  })
 
-	const lasagne = _.curry(function(firstIngredient, secondIngredient) {
-		return firstIngredient + ' ' + secondIngredient;
-	})
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('pasta')}, 1000);
+  });
 
-  	const promise = new Promise((resolve, reject) => {
-  		setTimeout(
-  		    () => {resolve('pasta')}, 
-  		    1000);
-  	});
-		
-		
-  	promise
-  		.then(lasagne(sauce))
-  		.then((result) => {
-  			console.log('2. yum!', result);
-	 	});
+  promise
+    .then(lasagne(sauce))
+    .then((result) => {
+      console.log('2. yum!', result);
+    });
 
 })();
 
 
-
 //EXPLANATION
 (function() {
+  const sauce = 'sauce';
 
-	const sauce = 'sauce';
+  const lasagne = function(firstIngredient) {
+    return (secondIngredient) => {
+      return firstIngredient + ' ' + secondIngredient;
+    }
+  }
 
-	const lasagne = function(firstIngredient) {
-		return (secondIngredient) => {
-			return firstIngredient + ' ' + secondIngredient;
-		}
-	}
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {resolve('pasta')}, 1000);
+  });
 
-  	const promise = new Promise((resolve, reject) => {
-  		setTimeout(
-  		    () => {resolve('pasta')}, 
-  		    1000);
-  	});
-		
-		
-  	promise
-  		.then(result => lasagne(sauce)(result))
-  		.then((result) => {
-  			console.log('3. yum!', result);
-	 	});
+  promise
+    .then(result => lasagne(sauce)(result))
+    .then((result) => {
+      console.log('3. yum!', result);
+    });
 
 })();
 
